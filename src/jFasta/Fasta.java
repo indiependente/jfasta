d407f69df3a796c7e7d88cba8a2940d694ec5592
gr1ph00n <gr1ph00n@gmail.com>
2014-07-02 17:34:55 +0200

    some refactoring plus added common words counter

--- src/jFasta/Fasta.java
+++ src/jFasta/Fasta.java
@@ -9,6 +9,7 @@ public class Fasta
 	private String reference, query;
 	
 	private HashMap<String, ArrayList<Integer>> lookupTableRef, lookupTableQuery;
+	private HashMap<String, Integer> commonWords; 
 	
 	public Fasta(int k, String s1, String s2)
 	{
@@ -17,6 +18,7 @@ public class Fasta
 		ktup = k;
 		lookupTableRef = new HashMap<String, ArrayList<Integer>>();
 		lookupTableQuery = new HashMap<String, ArrayList<Integer>>();
+		commonWords = new HashMap<String, Integer>();
 	}
 	
 	public void execute()
@@ -49,24 +51,34 @@ public class Fasta
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void findKTuples(String str, HashMap<String, ArrayList<Integer>> actualTable)
+	{
+		int n = str.length();
+		for (int i = 0; i < (n - ktup + 1); i++)
+		{
+			String sub = str.substring(i, i + ktup);
+			if(!actualTable.containsKey(sub))
+			{
+				actualTable.put(sub, new ArrayList<Integer>(i));
+			}
+			else
+			{
+				actualTable.get(sub).add(i);
+			}
+		}
+	}
 
 	private void findKTuples() 
 	{
-		HashMap<String, ArrayList<Integer>> actualTable;
-		
-		for (String str : new String[] { reference, query })
+		findKTuples(reference, lookupTableRef);
+		findKTuples(query, lookupTableQuery);
+		for (String str : lookupTableRef.keySet())
 		{
-			actualTable = (str==reference) ? lookupTableRef : lookupTableQuery;
-			int n = str.length();
-			for (int i = 0; i < (n - ktup + 1); i++)
+			if (lookupTableQuery.containsKey(str))
 			{
-				String sub = str.substring(i, i + ktup);
-				if(!actualTable.containsKey(sub)){
-					actualTable.put(sub, new ArrayList<Integer>(i));
-				}
-				else{
-					actualTable.get(sub).add(i);
-				}
+				int times = lookupTableRef.get(str).size() * lookupTableQuery.get(str).size();
+				commonWords.put(str, times);
 			}
 		}
 	}
