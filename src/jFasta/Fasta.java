182cc07cf30b833a0ebd0a47936bda3414f5657c
gr1ph00n <gr1ph00n@gmail.com>
2014-07-02 19:06:49 +0200

    implemented steps 1-2

--- src/jFasta/Fasta.java
+++ src/jFasta/Fasta.java
@@ -1,7 +1,10 @@
 package jFasta;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class Fasta 
 {
@@ -10,6 +13,26 @@ public class Fasta
 	
 	private HashMap<String, ArrayList<Integer>> lookupTableRef, lookupTableQuery;
 	private HashMap<String, Integer> commonWords; 
+	private TreeMap<Integer, Integer> sumTable;
+	
+
+	public static class DiagonalComparator implements Comparator<Integer>
+	{
+		private TreeMap<Integer, Integer> ref;
+		
+		public void setDiagonalComparator(TreeMap<Integer, Integer> map)
+		{
+			ref = map;
+		}
+
+		@Override
+		public int compare(Integer a, Integer b) 
+		{
+			return b - a;
+		}
+		
+	}
+	
 	
 	public Fasta(int k, String s1, String s2)
 	{
@@ -19,12 +42,16 @@ public class Fasta
 		lookupTableRef = new HashMap<String, ArrayList<Integer>>();
 		lookupTableQuery = new HashMap<String, ArrayList<Integer>>();
 		commonWords = new HashMap<String, Integer>();
+		DiagonalComparator dc = new DiagonalComparator();
+		sumTable = new TreeMap<Integer, Integer>(dc);
+		dc.setDiagonalComparator(sumTable);
 	}
 	
 	public void execute()
 	{
 		findKTuples();
-		sortKTuples();
+		makeSubTable();
+		sortDiagonals();
 		applySubstitutionMatrix();
 		joinDiagonals();
 		performSmithWaterman();
@@ -32,6 +59,33 @@ public class Fasta
 	
 	
 	
+	private void makeSubTable() 
+	{
+		// n + m - (2 * (k - 1) + 1)
+		// parallelizzare?
+		// unire punto 4 ? ricald
+		for (String str : commonWords.keySet())
+		{
+			ArrayList<Integer> listRef = lookupTableRef.get(str);
+			ArrayList<Integer> listQuery = lookupTableQuery.get(str);
+			for (int j : listRef)
+			{
+				for (int i : listQuery)
+				{
+					int diagonalId = i - j;
+					if (!sumTable.containsKey(diagonalId))
+						sumTable.put(diagonalId, 0);
+					sumTable.put(diagonalId, sumTable.get(diagonalId) + 1);
+					System.out.println("[" + i + "-" + j + "] " + diagonalId + " :> " + sumTable.get(diagonalId));
+				}
+			}
+			
+		}
+		for (Entry<Integer, Integer> e : sumTable.entrySet())
+			System.out.println("-->> " + e.getKey() + " ..:::> " + e.getValue());
+
+	}
+
 	private void performSmithWaterman() {
 		// TODO Auto-generated method stub
 		
@@ -47,8 +101,9 @@ public class Fasta
 		
 	}
 
-	private void sortKTuples() {
-		// TODO Auto-generated method stub
+	private void sortDiagonals() 
+	{
+		
 		
 	}
 	
@@ -58,13 +113,17 @@ public class Fasta
 		for (int i = 0; i < (n - ktup + 1); i++)
 		{
 			String sub = str.substring(i, i + ktup);
+			System.out.println(sub);
 			if(!actualTable.containsKey(sub))
 			{
-				actualTable.put(sub, new ArrayList<Integer>(i));
+				actualTable.put(sub, new ArrayList<Integer>());
+				actualTable.get(sub).add(i);
+				System.out.println(sub + ">>" + i);
 			}
 			else
 			{
 				actualTable.get(sub).add(i);
+				System.out.println(sub + ">>" + i);
 			}
 		}
 	}
@@ -79,13 +138,14 @@ public class Fasta
 			{
 				int times = lookupTableRef.get(str).size() * lookupTableQuery.get(str).size();
 				commonWords.put(str, times);
+				System.out.println(str + " : " + times);
 			}
 		}
 	}
 
 	public static void main(String[] args) 
 	{
-		Fasta fas = new Fasta(10, "antonio", "casino");
+		Fasta fas = new Fasta(2, "CCATCGCCATCG", "GCATCGGC");
 		fas.execute();
 	}
 
